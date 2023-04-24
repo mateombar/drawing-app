09cb04f42a803da6961ce44508d53719fe9fdac0
Mateo Montanez Baron <mateomontabaron@gmail.com>
2023-04-24 10:28:41 +0200

    Samples

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -46,6 +46,11 @@ fileNames.forEach((fn) => {
 // Write the samples array into our simples file
 fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
 
+fs.writeFileSync(
+  constants.SAMPLES_JS,
+  "const samples=" + JSON.stringify(samples) + ";"
+);
+
 function generateImageFile(outFile, paths) {
   // Clear canvas before drawing
   ctx.clearRect(0, 0, canvas.width, canvas.height);
